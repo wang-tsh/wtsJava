0a0bd89a22bfcbaceb8991453143e6f853e4d11b
wangtsh <wang-tsh@neusoft.com>
2020-01-06 22:31:25 +0800

    first commit

---
+++ src/main/java/com/wts/www/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.wts.www;
+
+import java.nio.charset.Charset;
+
+/**
+ * @author wangtingshun
+ * @Date 2019-12-27 - 11:00
+ */
+public  class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("Hello every body");
+        Integer integer = 1;
+        byte minByte = -128;
+        Byte byte1=1;
+        System.out.println(Charset.defaultCharset());
+
+    }
+}
