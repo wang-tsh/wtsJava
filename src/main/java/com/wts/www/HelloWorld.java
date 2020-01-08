e7cdbcca6c7dbcb50569a9d840711c31011c669a
wangtsh <wang-tsh@neusoft.com>
2020-01-08 14:43:49 +0800

    add log

--- src/main/java/com/wts/www/HelloWorld.java
+++ src/main/java/com/wts/www/HelloWorld.java
@@ -1,19 +1,23 @@
 package com.wts.www;
 
-import java.nio.charset.Charset;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author wangtingshun
  * @Date 2019-12-27 - 11:00
  */
+@Slf4j
 public  class HelloWorld
 {
+
     public static void main(String[] args) {
-        System.out.println("Hello every body");
-        Integer integer = 1;
-        byte minByte = -128;
-        Byte byte1=1;
-        System.out.println(Charset.defaultCharset());
+        log.info("Hello every body");
+        int count = 5;
+        float price =1.5F;
+        float totle=count*price;
+        log.debug("totle is " +totle);
+
 
     }
 }
